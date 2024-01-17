03647a899c5073b2614183af9df4626da06a41a8
luqmanrefidluke <156885004+luqmanrefidluke@users.noreply.github.com>
2024-01-17 15:19:33 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -12,11 +12,16 @@ if option=='line chart':
       np.random.randn(20, 3),
       columns=['a', 'b', 'c'])
     st.line_chart(chart_data)
-
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
-if show:
-    st.write(pd.DataFrame({
-        'Students': ['John', 'Lofa', 'Siti', 'Amy', 'Luke', 'Herda', 'Gajen'],
-        'Attendance Status': ['no', 'yes', 'no', 'no', 'yes', 'yes','yes']
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [3.1242, 101.6861],
+    columns=['lat', 'lon'])
+    st.map(map_data)
+elif option=='T n C':
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+            'Students': ['John', 'Lofa', 'Siti', 'Amy', 'Luke', 'Herda', 'Gajen'],
+            'Attendance Status': ['no', 'yes', 'no', 'no', 'yes', 'yes','yes']
 }))
